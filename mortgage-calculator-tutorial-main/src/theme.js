c0179e0216e5fdcfa2ef8622a7e455cb0634dc7d
Nupoor Kedare <143624372+nupoorkedare2002@users.noreply.github.com>
2023-10-13 12:49:46 +0530

    Add files via upload

---
+++ mortgage-calculator-tutorial-main/src/theme.js
@@ -0,0 +1,16 @@
+import { colors } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    h1: {
+      fontSize: "50px",
+      fontFamily: "fantasy",
+
+
+    }
+  }
+});
